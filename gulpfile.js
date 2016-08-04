f86071200ea2b685d3d3a5cc43ec6275e88f9694
omar.chacin <omar.chacin@sigmagroup.no>
2016-08-04 13:53:29 +0200

    Form added

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,8 @@ var config = {
         images: './src/images/*',
         css   : [
           'node_modules/bootstrap/dist/css/bootstrap.min.css',
-          'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+          'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+          'node_modules/bootstrap/dist/css/toastr.css'
         ],
         dist  : './dist',
         mainJs: './src/main.js'
